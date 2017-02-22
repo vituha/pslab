d07e6580925df46dd1bc7a0f29584019573e1b64
vituha <spam@vituha.net>
2017-02-22 03:12:09 +0200

    finalizing the lab 6A

--- Ch6/Lab_6A.ps1
+++ Ch6/Lab_6A.ps1
@@ -1,1 +1,31 @@
-Get-CimInstance -ComputerName localhost -ClassName Win32_ComputerSystem
+Function GetPasswordStatusString([int] $passwordStatus) 
+{
+    Switch ($passwordStatus) 
+    {
+        1 {return 'Disabled'}
+        2 {return 'Enabled'}
+        3 {return 'NA'}
+        4 {return 'Unknown'}
+        default {return 'lazha'}
+    }
+}
+
+Function GetOSInfo([string[]] $computerNames)
+{
+    $computerNames `
+    | select -Property `
+        @{Name='csinfo'; Expression={Get-CimInstance -ComputerName $_ -ClassName Win32_ComputerSystem}}, `
+        @{Name='biosinfo'; Expression={Get-CimInstance -ComputerName $_ -ClassName Win32_BIOS}}, `
+        @{Name='osinfo'; Expression={Get-CimInstance -ComputerName $_ -ClassName Win32_OperatingSystem}} `
+    | select -Property `
+        @{Name='ComputerName'; Expression={$_.csinfo.Name}}, `
+        @{Name='Workgroup'; Expression={$_.csinfo.Workgroup}}, `
+        @{Name='AdminPassword'; Expression={GetPasswordStatusString -passwordStatus $_.csinfo.AdminPassworStatus}}, `
+        @{Name='Model'; Expression={$_.csinfo.Model}}, `
+        @{Name='Manufacturer'; Expression={$_.csinfo.Manufacturer}}, `
+        @{Name='BIOSSerial'; Expression={$_.biosinfo.SerialNumber}}, `
+        @{Name='OSVersion'; Expression={$_.osinfo.Version}}, `
+        @{Name='SPVersion'; Expression={$_.osinfo.ServicePackMajorVersion}}
+}
+
+GetOSInfo -computerNames localhost
